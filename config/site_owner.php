e2ca285f7035e78366e2ad37c9eae03297f837d6
Tomas Uksas <uksas@hotmail.com>
2023-01-20 15:50:50 +0200

    first commit

---
+++ config/site_owner.php
@@ -0,0 +1,7 @@
+<?php
+//Settings to do with the identification of the site owner
+define('WEBSITE_NAME', 'Smaljob');
+define('OUR_NAME', 'Tomas Uksas');
+define('OUR_TELNUM', '');
+define('OUR_ADDRESS', '');
+define('OUR_EMAIL_ADDRESS', '');
